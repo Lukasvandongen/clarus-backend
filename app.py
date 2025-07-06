57fe56a851504d5f2f43bed82744f6181e1522de
BuildTools <unconfigured@null.spigotmc.org>
2025-07-06 20:11:08 +0200

    CORS app toevoegen, heel env valt weg GG's

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://degrondvraag.vercel.app"])
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
